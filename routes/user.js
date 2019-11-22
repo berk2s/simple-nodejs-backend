e1a715895d8675694e90743a11824124318e8f0a
Berk <berkk@Berks-MacBook-Pro.local>
2019-11-22 05:20:24 +0300

    heroku

--- routes/user.js
+++ routes/user.js
@@ -81,7 +81,7 @@ router.put('/update', async (req, res) => {
     const { userName, phone, userID } = req.body;
 
     try {
-        const update = await Order.findByIdAndUpdate({_id: userID}, { "$set": { username: userName, phone: phone }}, {new: true});
+        const update = await Order.findByIdAndUpdate({_id: userID}, { "$set": { "username": userName, "phone": phone }}, {new: true});
         res.json({
             message:'update is succesfull! veriler:'+userName+' '+phone+' '+userID,
             status:{
